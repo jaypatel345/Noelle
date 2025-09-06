3e70af0e5568061546c71775344dfc5ec544685c
Jay patel <77409923+jaypatel345@users.noreply.github.com>
2025-09-07 02:05:17 +0530

    new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8bd0383 (Initial commit)
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,13 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-<<<<<<< HEAD
-        {children}
-=======
         
         {children}
         
->>>>>>> 8bd0383 (Initial commit)
       </body>
     </html>
   );
